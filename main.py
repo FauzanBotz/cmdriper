36e844c3f13d45afdb336fff1ce2764651921ec5
FERDI Z-afk <69185890+FERDIZ-afk@users.noreply.github.com>
2021-09-06 05:19:08 +0700

    Up

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ chrome_options.binary_location = os.environ.get("/app/.apt/usr/bin/google-chrome
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("/app/.chromedriver/bin/chromedriver"), chrome_options=chrome_options)
+driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
 
 driver.get("https://en.ephoto360.com/write-text-on-wet-glass-online-589.html")
 text = driver.find_element_by_xpath('/html/body/div[1]/div/section/div/div[1]/form/ul/li[1]/div/div/input')
@@ -16,5 +16,5 @@ driver.find_element_by_xpath('/html/body/div[1]/div/section/div/div[1]/form/ul/l
 time.sleep(3)
 result = driver.find_element_by_class_name("bg-image").get_attribute("src")
 print(result)
-
+print("berhasil")
 driver.close()
