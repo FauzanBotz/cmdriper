f7a6189302e3efda466286f59f2d576e3fdc26c8
FERDI Z-afk <69185890+FERDIZ-afk@users.noreply.github.com>
2021-09-06 05:20:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import time
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("/app/.apt/usr/bin/google-chrome")
+#chrome_options.binary_location = os.environ.get("/app/.apt/usr/bin/google-chrome")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
